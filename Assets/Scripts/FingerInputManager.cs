f5b0a3cccda098cacfa088f467de0d316481be7a
HotdogsCC <chacha9669@hotmail.com>
2025-02-11 19:20:39 +1100

    tv model + finger input system progress

---
+++ Assets/Scripts/FingerInputManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerInputManager : MonoBehaviour
+{
+    private Minigame currentMinigame;
+
+    private KeyCode[] keyCodes = {KeyCode.None, KeyCode.None, KeyCode.None, KeyCode.None};
+
+    public void SetInput(int num, KeyCode key)
+    {
+        keyCodes[num] = key;
+    }
+
+    private void Update()
+    {
+        if(currentMinigame != null)
+        {
+            //sends out inputs
+            if(Input.GetKey(keyCodes[0]))
+            {
+                currentMinigame.Input1();
+            }
+            if (Input.GetKey(keyCodes[1]))
+            {
+                currentMinigame.Input2();
+            }
+            if (Input.GetKey(keyCodes[2]))
+            {
+                currentMinigame.Input3();
+            }
+            if (Input.GetKey(keyCodes[3]))
+            {
+                currentMinigame.Input4();
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Minigame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Minigame : MonoBehaviour
+{
+    public abstract void Input1();
+    public abstract void Input2();
+    public abstract void Input3();
+    public abstract void Input4();
+}
